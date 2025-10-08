26d68ca396faaf714a379cc73ff4c824ec116721
Sorokin Alexandr <sashasorokin754@gmail.com>
2025-10-08 17:43:28 +0300

    Добавьте файлы проекта.

---
+++ Lib/Threads.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <windows.h>
+
+DWORD WINAPI marker(LPVOID lpParam) {
+
+}
---
+++ OS_lab3/OS_lab3.cpp
@@ -0,0 +1,12 @@
+﻿// OS_lab3.cpp: определяет точку входа для приложения.
+//
+
+#include "OS_lab3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
